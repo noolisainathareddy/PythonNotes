7ec315aa0cce0d37c2e1412581f13724416188bc
Sai natha reddy Nooli <nooli.sainathareddy@gmail.com>
2025-07-28 22:30:42 -0500

    FileHandling

---
+++ FileHandling/ReadFile.py
@@ -0,0 +1,12 @@
+
+
+try:
+    open_file = open('DmmyData.txt', 'r')
+    print(open_file.read())
+except Exception as e:
+    print("Encountered error", e)
+
+finally:
+    print("Closing file")
+    open_file.close()
+
